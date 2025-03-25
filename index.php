6a1530ca307b95ca9725fc7a31fb7e1858ad1ac5
Dasun Sandepa <dsk.sandeep.987@gmail.com>
2025-03-25 11:03:11 +0530

    updated

--- index.php
+++ index.php
@@ -4,5 +4,26 @@ echo "hello","world";
 echo "hello"."world";
 echo "hello","world";
 print ("Hello World");
+$total = 3+/*2*/+2+1;
+echo " <!--  Total is $total --> ";
+echo $total;
 ?>
 <?= "Hello World"; ?>
+<!-- 
+hello -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1><?php echo "hello" ?></h1>
+  <p <?php echo 'style="color: red;"'?>>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    In commodi, nulla dignissimos iste facilis est repellat vitae. 
+    Autem at facilis quibusdam quas! Quibusdam vel mollitia possimus voluptatibus
+    placeat dignissimos qui!</p>
+</body>
+</html>
