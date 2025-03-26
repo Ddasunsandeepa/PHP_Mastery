c2691d30ddda30f6da7736079d0b939b1a24b17b
Dasun Sandepa <dsk.sandeep.987@gmail.com>
2025-03-26 09:55:38 +0530

    updated

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
-echo"Hello World";
+echo "<h1>Hello World</h1>";
+echo"Hello     World";
 echo "hello","world";
 echo "hello"."world";
 echo "hello","world";
+echo "<script>alert('123')</script>";
+echo 'alert("123")';
 print ("Hello World");
 $total = 3+/*2*/+2+1;
 echo " <!--  Total is $total --> ";
@@ -21,9 +24,15 @@ hello -->
 </head>
 <body>
   <h1><?php echo "hello" ?></h1>
+  <?php
+  echo "<h1>hello</h1>";
+  ?>
   <p <?php echo 'style="color: red;"'?>>Lorem ipsum dolor sit amet consectetur adipisicing elit.
     In commodi, nulla dignissimos iste facilis est repellat vitae. 
     Autem at facilis quibusdam quas! Quibusdam vel mollitia possimus voluptatibus
     placeat dignissimos qui!</p>
+    <script>
+      <?php echo 'alert("hello")'; ?>
+    </script>
 </body>
 </html>
